a633fbf107c57aaac0874aab4851e48adb492802
Charlie Gruenwald <cgruenwa@amazon.com>
2023-10-27 20:48:37 -0600

    [res/service_account] Add service account group to users ou. Update ACL path.

--- recipes/res/res_demo_env/assets/service_account.ps1
+++ recipes/res/res_demo_env/assets/service_account.ps1
@@ -24,19 +24,19 @@ catch [Microsoft.ActiveDirectory.Management.ADException]{
 
 try {
     # Add Service Account to Group that will have permission to write computers
-    Add-ADGroupMember -Identity "CN=$computer_write_group,$computers_ou" -Members "$service_account_name"
+    Add-ADGroupMember -Identity "CN=$computer_write_group,$users_ou" -Members "$service_account_name"
 
     # Look up SID and acl for Computers OU
     $computers = Get-ADOrganizationalUnit -Identity "$computers_ou"
     $group = Get-ADGroup -Identity "CN=$computer_write_group,$users_ou"
     $sid = new-object System.Security.Principal.SecurityIdentifier $group.SID
-    $acl = get-acl $computers
+    $acl = get-acl "AD:$computers"
 
     # Allow writes to computers
     $computers_schema_id = new-object Guid bf967a86-0de6-11d0-a285-00aa003049e2
     $ace = new-object System.DirectoryServices.ActiveDirectoryAccessRule $sid,"CreateChild","Allow",$computers_schema_id
     $acl.AddAccessRule($ace)
-    Set-Acl -AclObject $acl -Path "$computers_ou"
+    Set-Acl -AclObject $acl -Path "AD:$computers_ou"
     echo "Success."
 }
 catch {
