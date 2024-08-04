7f7d497dd1293a1b2d2aacafcfa11fdf2e13dee1
Matt Vaughn <mwvaughn@amazon.com>
2024-08-04 09:50:54 -0400

    Clean up deprecated recipes

--- recipes/pcs/pcs_demo/assets/job.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#SBATCH -J test
-#SBATCH -o test-%j.out   # Write the standard output to file named 'test-<job_number>.out'
-#SBATCH -e test-%j.err   # Write the standard error to file named 'test-<job_number>.err'
-
-echo "Hello world. This is job ${SLURM_JOB_NAME} [${SLURM_JOB_ID}] running on ${SLURMD_NODENAME}, submitted from ${SLURM_SUBMIT_HOST}" && sleep 60 && echo "Job complete"
--- recipes/pcs/pcs_demo/assets/s3-user-data-example.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-set -ex
-
-echo "Hello, world" > /var/log/${0}.log
