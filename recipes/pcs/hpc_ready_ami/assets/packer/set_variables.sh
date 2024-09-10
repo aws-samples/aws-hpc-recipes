c5f5ee8cb5769526ac5e0fb8dddea46df4258035
Matt Vaughn <mwvaughn@amazon.com>
2024-09-10 15:51:00 -0400

    README for the recipe. Improvements to Packer setup.

--- recipes/pcs/hpc_ready_ami/assets/packer/set_variables.sh
+++ recipes/pcs/hpc_ready_ami/assets/packer/set_variables.sh
@@ -6,17 +6,17 @@ ROOT_DEVICE_NAME="/dev/sda1"
 
 # Determine the correct values based on the distribution
 case "$1" in
-    amzn*)
+    amzn_2*)
         SSH_USERNAME="ec2-user"
         ROOT_DEVICE_NAME="/dev/xvda"
         ;;
-    ubuntu*)
+    ubuntu_22_04*)
         SSH_USERNAME="ubuntu"
         ;;
-    rocky*)
+    rocky_9*)
         SSH_USERNAME="rocky"
         ;;
-    rhel*)
+    rhel_9*)
         SSH_USERNAME="ec2-user"
         ;;
     *)
@@ -28,5 +28,6 @@ esac
 # Output the values in a format Packer can use
 echo "{"
 echo "  \"ssh_username\": \"$SSH_USERNAME\","
+echo "  \"distribution\": \"$1\","
 echo "  \"root_device_name\": \"$ROOT_DEVICE_NAME\""
 echo "}"
