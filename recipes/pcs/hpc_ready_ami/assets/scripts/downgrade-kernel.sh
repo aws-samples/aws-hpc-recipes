f65d0448d93d3e9d0293f2262fa614cd827068b6
Matt Vaughn <mwvaughn@amazon.com>
2024-11-17 15:30:44 -0500

    Install version-pinned Lustre driver to go along with downgraded kernel

--- recipes/pcs/hpc_ready_ami/assets/scripts/downgrade-kernel.sh
+++ recipes/pcs/hpc_ready_ami/assets/scripts/downgrade-kernel.sh
@@ -38,6 +38,8 @@ handle_ubuntu_22.04() {
     sudo sed -i 's/^GRUB_DEFAULT=.*$/GRUB_DEFAULT="Advanced options for Ubuntu>Ubuntu, with Linux '"${KERNEL_VERSION}"'"/' /etc/default/grub
     # Update GRUB
     sudo update-grub
+    # Remove 6.8x kernel
+    sudo dpkg -l | grep linux | grep aws | grep "6.8" | awk '{print $2}' | xargs apt remove -y || true
 
     # Hold kernel packages to prevent updates
     sudo apt-mark hold linux-image-${KERNEL_VERSION} linux-headers-${KERNEL_VERSION}
@@ -57,26 +59,6 @@ Pin: version 6.5.0*
 Pin-Priority: 1001
 EOF
 
-}
-
-handle_ubuntu_22.04_broken() {
-    logger "Updating Ubuntu 22.04" "INFO"
-    # 6.5.0-1024-aws is latest suppported by Lustre as of 2024.11.01
-    # See https://docs.aws.amazon.com/fsx/latest/LustreGuide/lustre-client-matrix.html for details
-    KERNEL_VERSION="6.5.0-1024-aws"
-    # Install specific kernel version
-    sudo apt update
-    sudo apt install -y linux-image-${KERNEL_VERSION} linux-headers-${KERNEL_VERSION}
-    # Hold kernel packages to prevent updates
-    sudo apt-mark hold linux-image-${KERNEL_VERSION} linux-headers-${KERNEL_VERSION} linux-aws
-    # Remove meta packages that might trigger updates
-    sudo apt remove -y linux-image-aws linux-headers-aws || true
-    # Clean up any automatically installed packages no longer needed
-    sudo apt autoremove -y
-    # Update GRUB default
-    sudo sed -i 's/^GRUB_DEFAULT=.*$/GRUB_DEFAULT="Advanced options for Ubuntu>Ubuntu, with Linux '"${KERNEL_VERSION}"'"/' /etc/default/grub
-    # Update GRUB
-    sudo update-grub
 }
 
 handle_rhel_9() { 
